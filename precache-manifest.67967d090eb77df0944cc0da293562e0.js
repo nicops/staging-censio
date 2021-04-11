17022f5e22e251d78540523f29d7651a663f87f2
Nicolás Perez Santoro <nicolas.perez.santoro@gmail.com>
2021-04-11 18:11:19 -0300

    init

---
+++ precache-manifest.67967d090eb77df0944cc0da293562e0.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef891a2821480570bcdcc6ca47eb961",
+    "url": "/staging-censio/index.html"
+  },
+  {
+    "revision": "796a9b4968af43564407",
+    "url": "/staging-censio/static/css/2.3dcccd17.chunk.css"
+  },
+  {
+    "revision": "09ce7e1333ad88293cfa",
+    "url": "/staging-censio/static/css/main.ad5bd9b9.chunk.css"
+  },
+  {
+    "revision": "796a9b4968af43564407",
+    "url": "/staging-censio/static/js/2.f08658b2.chunk.js"
+  },
+  {
+    "revision": "5c84fcf47627ef219210e03cfa950dcb",
+    "url": "/staging-censio/static/js/2.f08658b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ce7e1333ad88293cfa",
+    "url": "/staging-censio/static/js/main.e0978fa0.chunk.js"
+  },
+  {
+    "revision": "f0ff3ca4fee9156d993f",
+    "url": "/staging-censio/static/js/runtime-main.54096f03.js"
+  },
+  {
+    "revision": "6aae84abf0090324eb1daa7eef6bfdd8",
+    "url": "/staging-censio/static/media/bootstrap-5-logo.6aae84ab.svg"
+  },
+  {
+    "revision": "0c9f225e8f69c622f681cf1ed973cc3d",
+    "url": "/staging-censio/static/media/fa-brands-400.0c9f225e.woff2"
+  },
+  {
+    "revision": "0ce1e868452204695c8ac1c70f693c2d",
+    "url": "/staging-censio/static/media/fa-brands-400.0ce1e868.woff"
+  },
+  {
+    "revision": "5d10d00980077f67990f73b1961bc46e",
+    "url": "/staging-censio/static/media/fa-brands-400.5d10d009.svg"
+  },
+  {
+    "revision": "d28c96cabe5302f1b2c90275cd3f4e69",
+    "url": "/staging-censio/static/media/fa-brands-400.d28c96ca.eot"
+  },
+  {
+    "revision": "dfe5aa4344a2d8a29aec8d83fb3fb14e",
+    "url": "/staging-censio/static/media/fa-brands-400.dfe5aa43.ttf"
+  },
+  {
+    "revision": "0232fe8b09de812c88e6cc27f57a3a88",
+    "url": "/staging-censio/static/media/fa-regular-400.0232fe8b.eot"
+  },
+  {
+    "revision": "398be22b2308585b84da8e858e2d7b60",
+    "url": "/staging-censio/static/media/fa-regular-400.398be22b.ttf"
+  },
+  {
+    "revision": "3cace4a04d941b5981ba32c6ce9afae1",
+    "url": "/staging-censio/static/media/fa-regular-400.3cace4a0.woff"
+  },
+  {
+    "revision": "847712aaabbeba674afdda86d31cab17",
+    "url": "/staging-censio/static/media/fa-regular-400.847712aa.woff2"
+  },
+  {
+    "revision": "d0fdc7c20b0b0b1aaf27327547735fa3",
+    "url": "/staging-censio/static/media/fa-regular-400.d0fdc7c2.svg"
+  },
+  {
+    "revision": "00bb62bc33034479d5e82a0fd1c5efe3",
+    "url": "/staging-censio/static/media/fa-solid-900.00bb62bc.eot"
+  },
+  {
+    "revision": "2306f642b31b774ba1d8b6b784539e19",
+    "url": "/staging-censio/static/media/fa-solid-900.2306f642.svg"
+  },
+  {
+    "revision": "260d033f79f75d670fd09d8cf7c8bf8d",
+    "url": "/staging-censio/static/media/fa-solid-900.260d033f.ttf"
+  },
+  {
+    "revision": "4bfbf7eb4b19d9ff9293eb177b6d0070",
+    "url": "/staging-censio/static/media/fa-solid-900.4bfbf7eb.woff"
+  },
+  {
+    "revision": "9ae050d1876ac1763eb6afe4264e6d5a",
+    "url": "/staging-censio/static/media/fa-solid-900.9ae050d1.woff2"
+  },
+  {
+    "revision": "4d26e151b224eaa88df2742351073c56",
+    "url": "/staging-censio/static/media/profile-picture-1.4d26e151.jpg"
+  },
+  {
+    "revision": "487133c5319652023f459ea4e3a4720a",
+    "url": "/staging-censio/static/media/profile-picture-2.487133c5.jpg"
+  },
+  {
+    "revision": "9a250e5ca0df93fd467f77c4d7e3d3d4",
+    "url": "/staging-censio/static/media/profile-picture-3.9a250e5c.jpg"
+  },
+  {
+    "revision": "ccd796b31c6ca0c778c6065b3b6a20b7",
+    "url": "/staging-censio/static/media/profile-picture-4.ccd796b3.jpg"
+  },
+  {
+    "revision": "baf7bf753ca889c7db833fb0db47abac",
+    "url": "/staging-censio/static/media/profile-picture-5.baf7bf75.jpg"
+  },
+  {
+    "revision": "9394d800b9cadc76bc0dd4ecb6f9edcf",
+    "url": "/staging-censio/static/media/react-hero-logo.9394d800.svg"
+  },
+  {
+    "revision": "f7d45c01a0efd126cc1030d5cf78fd84",
+    "url": "/staging-censio/static/media/react-logo-transparent.f7d45c01.svg"
+  },
+  {
+    "revision": "b779e4fd2515e28f0233a071f3d46104",
+    "url": "/staging-censio/static/media/react-logo.b779e4fd.svg"
+  },
+  {
+    "revision": "58ee1d5a0a93d00df3f6c12170ebd3ff",
+    "url": "/staging-censio/static/media/signin.58ee1d5a.svg"
+  },
+  {
+    "revision": "105e5059b0997718976601d17fd2a04b",
+    "url": "/staging-censio/static/media/themesberg.105e5059.svg"
+  }
+]);
